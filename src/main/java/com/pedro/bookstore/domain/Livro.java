fa1a9a1713fa3f8a6d44031d9209f4744daf05c4
Pedrox077 <pedrovieira7070@gmail.com>
2023-10-27 18:22:28 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/pedro/bookstore/domain/Livro.java
@@ -0,0 +1,44 @@
+package com.pedro.bookstore.domain;
+
+import java.util.Objects;
+
+public class Livro {
+
+	private Integer id;
+	private String titulo;
+	private String nome_autor;
+	private String texto;
+
+	private Categoria categoria;
+
+	public Livro() {
+		super();
+	}
+
+	public Livro(Integer id, String titulo, String nome_autor, String texto, Categoria categoria) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.nome_autor = nome_autor;
+		this.texto = texto;
+		this.categoria = categoria;
+
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(id, other.id);
+	}
+}
